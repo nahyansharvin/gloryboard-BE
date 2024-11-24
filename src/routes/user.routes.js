05457461d767eacf46ba7b1cd16e9d8af4a9f30d
Aseel <aseel@lektik.com>
2024-11-24 16:57:42 +0530

    update : add fetch event API to public route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,9 +16,11 @@ import {
   fetchAllResults,
   fetchResultById,
   fetchResultByEventId,
-  fetchAllGroupResults,
   fetchAllIndividualResults,
+  fetchLeaderboard,
 } from "../controllers/result.controller.js";
+
+import { fetchAllEvents } from "../controllers/event.controller.js";
 
 import { verifyJWT, verifyRole } from "../middlewares/auth.middlewares.js";
 import { DEPARTMENTS } from "../constants.js";
@@ -30,13 +32,15 @@ router.route("/registerAdmin").post(registerAdmin);
 router.route("/login").post(loginUser);
 router.route("/").get(fetchAllUsers);
 
+router.route("/events").get(fetchAllEvents);
+
 router.route("/results").get(fetchAllResults);
 router.route("/result/:id").get(fetchResultById);
 router.route("/result/event/:event_id").get(fetchResultByEventId);
 
-router.route("/results/group").get(fetchAllGroupResults);
+// router.route("/results/group").get(fetchAllGroupResults);
 router.route("/results/single").get(fetchAllIndividualResults);
-
+router.route("/results/leaderboard").get(fetchLeaderboard);
 
 // Protucted Router
 router.route("/register").post(verifyJWT, verifyRole(["admin"]), registerUser);
@@ -50,6 +54,8 @@ router
   .route("/delete")
   .get(verifyJWT, verifyRole(["admin", "rep"]), deleteUserById);
 
-router.route("/departments").get(verifyJWT, verifyRole(["admin", "rep"]), fetchDepartments);
+router
+  .route("/departments")
+  .get(verifyJWT, verifyRole(["admin", "rep"]), fetchDepartments);
 
 export default router;
